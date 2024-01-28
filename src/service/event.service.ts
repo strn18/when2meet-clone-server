b23ff6125677e2a6e9b60c041d20fe0af6c6b4cf
strn18 <strn18@korea.ac.kr>
2024-01-28 11:14:55 +0900

    ADD: event service를 추가하고, event 저장 및 랜덤 url 생성을 위한 함수 추가

---
+++ src/service/event.service.ts
@@ -0,0 +1,29 @@
+import Event from '../entity/event.entity';
+import EventRepository from '../repository/event.repository';
+import CreateEventInput from '../type/event/create.input';
+import { InternalServerError } from '../util/customErrors';
+
+export default class EventService {
+  static async saveEvent(createEventInput: CreateEventInput): Promise<Event> {
+    try {
+      const eventEntity = await EventRepository.create(createEventInput);
+
+      return await EventRepository.save(eventEntity);
+    } catch (error) {
+      throw new InternalServerError('이벤트 저장을 실패했습니다.');
+    }
+  }
+
+  static createRandomString(length: number): string {
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456879';
+
+    let result = '';
+
+    for (let i = 0; i < length; i++) {
+      result += characters[Math.floor(Math.random() * characters.length)];
+    }
+
+    return result;
+  }
+}
