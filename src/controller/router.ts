8ec4d8b5ef84a6137da4d70f91aee4b6411d0b57
strn18 <strn18@korea.ac.kr>
2024-01-28 11:18:10 +0900

    MOD: event router 연결

--- src/controller/router.ts
+++ src/controller/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userRouter from './user/router';
+import eventRouter from './event/router';
 
 const router = Router();
 
 router.use('/user', userRouter);
+router.use('/event', eventRouter);
 
 export default router;
