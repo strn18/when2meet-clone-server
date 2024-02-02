03598415de085b8ace65abc16b833083c3599973
strn18 <strn18@korea.ac.kr>
2024-02-02 12:25:51 +0900

    MOD: POST /user/:url 추가에 따른 라우터 연결

--- src/controller/user/router.ts
+++ src/controller/user/router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { getUserVotes, getUserById } from './controller';
+import { getUserVotes, getUserById, saveUserVotes } from './controller';
 
 const userRouter = Router();
 
 userRouter.get('/:url', getUserVotes);
+userRouter.post('/:url', saveUserVotes);
+
 userRouter.get('/', getUserById);
 // query를 사용하여 id에 맞는 user 정보를 가져오는 api ex) GET http://localhost:3000/user?id=1
 // userRouter.get('/:age', getUsersByAge); // param을 사용하여 age에 맞는 user들 정보를 가져오는 api ex) GET http://localhost:3000/user/23
